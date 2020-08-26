be12490340dde048c8c53389460493df5d41b325
Elad Becker <becker.elad@gmail.com>
2020-08-26 14:29:40 +0300

    first upload of keep-app

--- apps/Keep/KeepApp.jsx
+++ apps/Keep/KeepApp.jsx
@@ -1,13 +1,27 @@
+import { notesService } from './services/notes-service.js'
+import { NoteList } from './NoteList.jsx'
 
 export class KeepApp extends React.Component {
-
+    state = {
+        notes: null
+    }
+    componentDidMount() {
+        this.loadNotes();
+    }
+    loadNotes = () => {
+        notesService.getAllNotes()
+            .then(notes => {
+                this.setState({ notes });
+            });
+    }
     render() {
+        const { notes } = this.state;
         return (
-        
-            <div>
-              <h1>Keep APP</h1>
-            </div>
-    
-        )
+            <main>
+                <h1>KEEP APP</h1>
+                {!notes && <span>Loading...</span>}
+                {notes && <NoteList notes={notes} />}
+            </main>
+        );
     }
 }
---
+++ apps/Keep/NoteVideo.jsx
@@ -0,0 +1,7 @@
+export class NoteVideo extends React.Component {
+    
+    render() {
+        console.log(this.props);
+        return <span>NoteVideo</span>
+    }
+}
