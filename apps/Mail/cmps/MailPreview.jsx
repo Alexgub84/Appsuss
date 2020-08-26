e5b19da65aa4f5a19cad6226e5025eede03380c7
Alexgit config --global user.email alex.gub@gmai.comgit config --global user.name Alex <alexgub@h-MacBook-Air-sl-Alex.local>
2020-08-26 18:06:03 +0300

    Alex befor class

--- apps/Mail/cmps/MailPreview.jsx
+++ apps/Mail/cmps/MailPreview.jsx
@@ -1,12 +1,21 @@
 
 export function MailPreview({mail}){
-    console.log(mail);
+
+    function getDate(){
+        const  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const date = new Date(mail.sentAt);
+        const dateToShow = date.getDay()+' '+ months[date.getMonth()];
+        return dateToShow
+    }
+    
+
     return (
         <li className="mail-preview">
+            <input type="checkbox" ></input>
             <h4>{mail.fromName}</h4>
             <h4>{mail.subject}</h4>
             <h4>Short body</h4>
-            <h4>{mail.sentAt}</h4>
+            <h4>{getDate()}</h4>
         </li>
     )
 }
