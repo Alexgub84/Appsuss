5e9f72f98372e0c0dea1e4b1161d79ddf6bfab2a
Elad Becker <becker.elad@gmail.com>
2020-08-29 03:38:38 +0300

    menu design and icons

--- cmps/NavBar.jsx
+++ cmps/NavBar.jsx
@@ -22,33 +22,45 @@ class _NavBar extends React.Component {
                         <span className="appsus-logo">&nbsp;| APPSUS</span>
                     </h1>
                     <div className="btn-nav-menu" onClick={this.toggleMenu}>☰</div>
-                    <nav className="nav-container" style={{ display: (isMenuHidden) ? 'none' : 'block' }}>
-                        {!isMenuHidden && <div>
+                    <nav className="nav-container" style={{ display: (isMenuHidden) ? 'none' : 'grid' }}>
+                        {!isMenuHidden && <React.Fragment>
                             <NavLink exact activeClassName='active-nav' onClick={this.toggleMenu} to='/'>
                                 <div className="nav-link">
+                                    {/* <i className="fas fa-home"></i> */}
+                                    <div className="img-container">
+                                        <img src="../assets/img/home-icon.png" />
+                                    </div>
                                     <h3>Home</h3>
-                                    <i className="fas fa-home"></i>
-                                </div>
-                            </NavLink>
-                            <NavLink exact onClick={this.toggleMenu} to="/keep">
-                                <div className="nav-link">
-                                    <h3>Keep</h3>
-                                    <i className="far fa-sticky-note"></i>
-                                </div>
-                            </NavLink>
-                            <NavLink exact onClick={this.toggleMenu} to="/mail">
-                                <div className="nav-link">
-                                    <h3>Mail</h3>
-                                    <i className="far fa-envelope"></i>
                                 </div>
                             </NavLink>
                             <NavLink exact onClick={this.toggleMenu} to="/about">
                                 <div className="nav-link">
+                                    {/* <i className="fas fa-info"></i> */}
+                                    <div className="img-container">
+                                        <img src="../assets/img/about-us-icon.jpg" />
+                                    </div>
                                     <h3>About</h3>
-                                    <i className="fas fa-info"></i>
                                 </div>
                             </NavLink>
-                        </div>}
+                            <NavLink exact onClick={this.toggleMenu} to="/mail">
+                                <div className="nav-link">
+                                    {/* <i className="far fa-envelope"></i> */}
+                                    <div className="img-container">
+                                        <img src="../assets/img/mail-icon.png" />
+                                    </div>
+                                    <h3>Mail</h3>
+                                </div>
+                            </NavLink>
+                            <NavLink exact onClick={this.toggleMenu} to="/keep">
+                                <div className="nav-link">
+                                    {/* <i className="far fa-sticky-note"></i> */}
+                                    <div className="img-container">
+                                        <img src="../assets/img/note-icon.png" />
+                                    </div>
+                                    <h3>Keep</h3>
+                                </div>
+                            </NavLink>
+                        </React.Fragment>}
                     </nav>
                 </section>
             </header>
